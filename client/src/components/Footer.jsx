192ebe3e196bb0d74b72a38e728ff0c5cb7595e2
Josué Aquino <aquinojosue889@gmail.com>
2024-09-08 23:53:20 -0300

    se realizo el footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import styles from "../css/footer.module.css";
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className={styles.footer}>
+      {/*parte arriba*/}
+      <div >
+      </div>
+      {/*parte central*/}
+      <div className={styles.footer__redes}>
+        <div className={styles.footer__redes__redSocial}>
+          <i className="bi bi-tiktok"></i>
+        </div>
+        <div className={styles.footer__redes__redSocial}>
+          <i className="bi bi-instagram"></i>
+        </div>
+        <div className={styles.footer__redes__redSocial}>
+          <i className="bi bi-facebook"></i>
+        </div>
+      </div>
+      {/*parte abajo*/}
+       <div className={styles.footer__copyright}>
+          <h5 className={styles.footer__copyright__text}>
+            Derechos reservados ©
+          </h5>
+       </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
