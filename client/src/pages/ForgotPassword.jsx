6651d354fe4a1f190a76834f6ed33d8397809db4
Josué Aquino <aquinojosue889@gmail.com>
2024-09-09 23:48:16 -0300

    se creo la pagina olvido de contraseña sin sus funciones

--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styles from "../css/registerForm.module.css";
 
 const ForgotPassword = () => {
-  return (
-    <div>ForgotPassword</div>
-  )
-}
+    const navigate = useNavigate();
 
-export default ForgotPassword
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const { email, password } = formData;
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            navigate("/");
+        } catch (error) {
+            throw error;
+        }
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className={styles.loginUser}>
+                <div className={styles.containerloginUser}>
+                    {/* titulo */}
+                    <div className={styles.loginUser__containerTitle}>
+                        <h1 className={styles.loginUser__title}>
+                            Usuario
+                        </h1>
+                        <div className={styles.loginUser__underline}></div>
+                    </div>
+                    {/* inputs */}
+                    <div className={styles.loginUser__containerInputs}>
+                        <div className={styles.loginUser__containerInput}>
+                            <i
+                                className={`bi bi-envelope-at-fill mx-1 ${styles.loginUser__iconInput}`}
+                            ></i>
+                            <input
+                                className={styles.loginUser__input}
+                                type="email"
+                                placeholder="email"
+                                id="emailRegister"
+                                name="email"
+                                value={email}
+                                onChange={handleInputChange}
+                            />
+                        </div>
+                        <div className={styles.loginUser__containerInput}>
+                            <i
+                                className={`bi bi-envelope-at-fill mx-1 ${styles.loginUser__iconInput}`}
+                            ></i>
+                            <input
+                                className={styles.loginUser__input}
+                                type="password"
+                                placeholder="contraseña"
+                                name="password"
+                                value={password}
+                                onChange={handleInputChange}
+                            />
+                        </div>
+                        <div className={styles.loginUser__containerInput}>
+                            <i
+                                className={`bi bi-envelope-at-fill mx-1 ${styles.loginUser__iconInput}`}
+                            ></i>
+                            <input
+                                className={styles.loginUser__input}
+                                type="password"
+                                placeholder="contraseña"
+                                name="password"
+                                value={password}
+                                onChange={handleInputChange}
+                            />
+                        </div>
+                    </div>
+                    {/* botones para cambiar contraseña*/}
+                    <div className={styles.loginUser__containerBtns}>
+                        <Link to={`/loginForm`}>
+                            <button className={styles.loginUser__button}>
+                                Cambiar contraseña
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </form>
+    );
+};
+
+export default ForgotPassword ;
